4fab1e6e129aa0fb6f44844bd9a3c47700e86b47
YU HOSHINO <yhoshino11@gmail.com>
2025-04-18 17:45:05 +0900

    Update index.ts

--- assets/index.ts
+++ assets/index.ts
@@ -1,10 +1,18 @@
+type XRSupportType = 'inline'
 type WebXR = {
   readonly xr: {
     // https://www.w3.org/TR/webxr/#xrsessionmode-enum
-    readonly isSessionSupported: (mode: 'inline') => Promise<boolean>
+    readonly isSessionSupported: (mode: XRSupportType) => Promise<boolean>
   }
 }
 
-(navigator as unknown as WebXR).xr.isSessionSupported("inline").then((status: boolean) => {
+async function getXRSupportStatus(mode: XRSupportType) {
+  return await (navigator as (Navigator | unknown) as WebXR).xr.isSessionSupported(mode)
+}
+
+function init() {
+  const status = getXRSupportStatus('inline')
   document.getElementsByClassName('isSessionSupported')[0].textContent = `${status}`
-})
+}
+
+init()
