8af55b918af4f24e36336ec9cec1250b9502b940
YU HOSHINO <yhoshino11@gmail.com>
2025-04-19 07:18:07 +0900

    例外処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Device } from "@/device"
 async function run() {
-  const status = await new Device().isXRSupported()
-  document.getElementsByClassName('isSessionSupported')[0].textContent = `${status}`
+  try {
+    const status = await new Device().isXRSupported()
+    document.getElementsByClassName('isSessionSupported')[0].textContent = `${status}`
+  } catch (e) {
+    document.getElementsByClassName('isSessionSupported')[0].textContent = `${e}`
+  }
 }
 run()
