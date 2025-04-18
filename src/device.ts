3888c9c48ea409d4cc0170c25e029090aa4d99be
YU HOSHINO <yhoshino11@gmail.com>
2025-04-19 07:20:15 +0900

    更新

--- src/device.ts
+++ src/device.ts
@@ -12,6 +12,9 @@ class Device implements IDevice {
      * [Reference W3C](https://www.w3.org/TR/webxr/#xrsessionmode-enum)
      */
     async isXRSupported(mode: XRMode = 'inline'): Promise<boolean> {
+        if (this.#navigator === undefined) {
+            return Promise.resolve(false)
+        }
         const status = await this.#navigator.xr.isSessionSupported(mode)
         return status
     }
