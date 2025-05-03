49537c970845de880a785118f9441b37a04c3658
YU HOSHINO <yhoshino11@gmail.com>
2025-05-04 01:24:07 +0900

    省略

--- bundle.js
+++ bundle.js
@@ -1,21 +1,13 @@
-"use strict";(()=>{var s=window.document;var n=window.navigator;function m(r){return!!r.featurePolicy.allowsFeature("xr-spatial-tracking","https://127.0.0.1:3000")}var o=new Map;o.set("inline",null);o.set("immersive-ar",null);o.set("immersive-vr",null);async function c(){let r=s.getElementById("root"),e=s.getElementsByClassName("console-dom").item(0);e.textContent+=`
+"use strict";(()=>{var s=window.document;var t=window.navigator;function l(i){return!!i.featurePolicy.allowsFeature("xr-spatial-tracking","https://127.0.0.1:3000")}var n=new Map;n.set("inline",null);n.set("immersive-ar",null);n.set("immersive-vr",null);async function c(){let i=s.getElementById("root"),e=s.getElementsByClassName("console-dom").item(0);e.textContent+=`
   \u6A5F\u80FD\u78BA\u8A8D
   `,e.textContent+=`
-  xr-spatial-tracking = ${m(r)}
-  `;let a=await n.xr.isSessionSupported("inline");e.textContent+=`
-  navigator.xr.isSessionSupported('inline') = ${a}
-  `;let u=await n.xr.isSessionSupported("immersive-ar");e.textContent+=`
-  navigator.xr.isSessionSupported('immersive-ar') = ${u}
-  `;let l=await n.xr.isSessionSupported("immersive-vr");e.textContent+=`
-  navigator.xr.isSessionSupported('immersive-vr') = ${l}
-  `,s.getElementsByClassName("enter inline").item(0).addEventListener("click",async i=>{if(i.isTrusted)try{let t=await n.xr.requestSession("inline",{requiredFeatures:["unbounded"],optionalFeatures:[]});e.textContent+=`
-      ${JSON.stringify(t)}
-      `}catch(t){e.textContent+=`
-      ${JSON.stringify(t)}`}}),s.getElementsByClassName("enter ar").item(0).addEventListener("click",async i=>{if(i.isTrusted)try{let t=await n.xr.requestSession("immersive-ar",{requiredFeatures:["unbounded"],optionalFeatures:[]});e.textContent+=`
-      ${JSON.stringify(t)}
-      `}catch(t){e.textContent+=`
-      ${JSON.stringify(t)}`}}),s.getElementsByClassName("enter vr").item(0).addEventListener("click",async i=>{if(i.isTrusted)try{let t=await n.xr.requestSession("immersive-vr",{requiredFeatures:["unbounded"],optionalFeatures:[]});e.textContent+=`
-      ${JSON.stringify(t)}
-      `}catch(t){e.textContent+=`
-      ${JSON.stringify(t)}
-      `}})}c();})();
+  xr-spatial-tracking = ${l(i)}
+  `;let r=await t.xr.isSessionSupported("inline");e.textContent+=`
+  navigator.xr.isSessionSupported('inline') = ${r}
+  `;let o=await t.xr.isSessionSupported("immersive-ar");e.textContent+=`
+  navigator.xr.isSessionSupported('immersive-ar') = ${o}
+  `;let a=await t.xr.isSessionSupported("immersive-vr");e.textContent+=`
+  navigator.xr.isSessionSupported('immersive-vr') = ${a}
+  `,s.getElementsByClassName("enter ar").item(0).addEventListener("click",async u=>{if(!u.isTrusted)return;let m=await t.xr.requestSession("immersive-ar",{requiredFeatures:["viewer"],optionalFeatures:[]});e.textContent+=`
+    ${m.enabledFeatures}
+    `})}c();})();
