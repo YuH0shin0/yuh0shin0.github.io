53d7cbea85e23db9bc4e8c1a3537d5f840941de3
YU HOSHINO <yhoshino11@gmail.com>
2025-05-04 02:00:10 +0900

    更新

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-"use strict";(()=>{var t=window.document;var e=window.navigator;var n=new Map;n.set("inline",null);n.set("immersive-ar",null);n.set("immersive-vr",null);async function m(){let i=t.getElementsByClassName("console-dom").item(0);i.textContent+=`
+"use strict";(()=>{var n=window.document;var e=window.navigator;var t=new Map;t.set("inline",null);t.set("immersive-ar",null);t.set("immersive-vr",null);async function m(){let i=n.getElementsByClassName("console-dom").item(0);i.textContent+=`
   \u6A5F\u80FD\u78BA\u8A8D
   `;let s=await e.xr.isSessionSupported("inline");i.textContent+=`
   navigator.xr.isSessionSupported('inline') = ${s}
@@ -6,6 +6,6 @@
   navigator.xr.isSessionSupported('immersive-ar') = ${r}
   `;let o=await e.xr.isSessionSupported("immersive-vr");i.textContent+=`
   navigator.xr.isSessionSupported('immersive-vr') = ${o}
-  `,t.getElementsByClassName("enter").item(0).addEventListener("click",async a=>{if(!a.isTrusted)return;let u=await e.xr.requestSession("immersive-ar",{requiredFeatures:["depth-sensing"],depthSensing:{usagePreference:["cpu-optimized","gpu-optimized"],dataFormatPreference:["luminance-alpha","float32"]}});i.textContent+=`
+  `,n.getElementsByClassName("enter").item(0).addEventListener("click",async a=>{if(!a.isTrusted)return;let u=await e.xr.requestSession("immersive-ar",{requiredFeatures:["depth-sensing","unbounded"],depthSensing:{usagePreference:["cpu-optimized","gpu-optimized"],dataFormatPreference:["luminance-alpha","float32"]}});i.textContent+=`
     ${u.enabledFeatures}
     `})}m();})();
